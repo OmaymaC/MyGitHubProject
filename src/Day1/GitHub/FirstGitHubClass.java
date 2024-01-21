154d58a81b777ea49337913b1b95802ef1134bff
OmaymaC <157265248+OmaymaC@users.noreply.github.com>
2024-01-21 17:18:29 -0500

    Update FirstGitHubClass.java
    "Hello" changed to "Hey"

--- src/Day1/GitHub/FirstGitHubClass.java
+++ src/Day1/GitHub/FirstGitHubClass.java
@@ -2,7 +2,7 @@ package Day1.GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
+        System.out.println("Hey GitHub!");
         System.out.println(1+1);
     }
 }
